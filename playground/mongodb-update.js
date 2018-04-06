4092a27a6ec38aa471e8e63c344e7b22ab71c9e1
Abdus Sadeque <asadeque@DPL-C02W25SVJ1GG.local>
2018-04-06 14:03:04 -0400

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect MongoDB Server.');
+  }
+  console.log('Congratulations! Connected to MongoDB Server.');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5ac79665e3b99b98ac9173c4')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5ac775acd634920e3d8861bd')
+  }, {
+    $set: {
+      name: 'Andrew'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
